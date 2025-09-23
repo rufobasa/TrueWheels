602fffa41a0010bde4f5e05b163bf742d39944cf
Alejandro Roby <aleroby@gmail.com>
2025-09-23 22:23:31 +0000

    add image field to cars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_23_200007) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_23_222135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_23_200007) do
     t.string "serial_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "chats", force: :cascade do |t|
