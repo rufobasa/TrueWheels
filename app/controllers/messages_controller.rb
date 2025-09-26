f40395fd30e48b5d21af940cf3d10516431a151a
Alejandro Roby <aleroby@gmail.com>
2025-09-26 21:04:40 +0000

    soy un pelotudo

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   @prompt_cars = Car.all.map(&:attributes).to_json
-  host = https"://true-wheels-d6fd7f4aeeb5.herokuapp.com"
+  host = "https://true-wheels-d6fd7f4aeeb5.herokuapp.com"
   SYSTEM_PROMPT = "Actúa como un vendedor de autos muy experto de un concesionario.
 
   Para dar informacion sobre autos, te limitarás a ofrecer exclusivamente los que están contenidos en: #{@prompt_cars}.
