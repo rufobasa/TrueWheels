3962aaa10458b158d0940bd522c78f499a421aff
Alejandro Roby <aleroby@gmail.com>
2025-09-26 01:13:07 +0000

    fix Message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,13 @@
 class MessagesController < ApplicationController
+
   @prompt_cars = Car.all.map(&:attributes).to_json
   SYSTEM_PROMPT = "seras un vendedor experto de autos, debes limitarte solo en la informcacion contenida en este prompt. Ademas construiras por cada vehiculo recomendado un link que tenga la siguiente forma: /cars/:id"
+
   #@prompt_cars = @cars.map do |car|
     #"[#{car.id},#{car.brand},#{car.miles},#{car.color},#{car.acceleration},#{car.consumption},#{car.fuel_type},#{car.transmission},#{car.car_model},#{car.version},#{car.doors},#{car.body_type},#{car.airbags},#{car.tank_capacity},#{car.services}#{car.occur_accident},#{car.serial_number}]"
   #end.join("[]")
   #@prompt_cars = @prompt_cars.join("[]")
+
   def create
     @chat = Chat.find(params[:chat_id])
     @message = Message.new(message_params.merge(role: "user", chat_id: @chat.id))
@@ -12,7 +15,7 @@ class MessagesController < ApplicationController
     # @message.role = "user"
     # @message.chat_id = @chat.id
     if @message.save
-      response = @chat.with_instruction(instructions).ask(@message.content)
+      response = RubyLLM.chat.with_instructions(instructions).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat_id: @chat.id)
       redirect_to chat_path(@chat)
     else
